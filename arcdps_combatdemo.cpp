bb81ef550b1a39ce6fbfc878b5314e9ec6416ff3
Coolkidgoodking <ashiro12138@gmail.com>
2021-02-09 22:42:31 +1000

    init test upload

---
+++ arcdps_combatdemo.cpp
@@ -0,0 +1,100 @@
+/*
+* arcdps combat api example
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <Windows.h>
+#include "imgui/imgui.h"
+
+/* arcdps export table */
+typedef struct arcdps_exports {
+	uintptr_t size; /* size of exports table */
+	uint32_t sig; /* pick a number between 0 and uint32_t max that isn't used by other modules */
+	uint32_t imguivers; /* set this to IMGUI_VERSION_NUM. if you don't use imgui, 18000 (as of 2021-02-02) */
+	const char* out_name; /* name string */
+	const char* out_build; /* build string */
+	void* wnd_nofilter; /* wndproc callback, fn(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) */
+	void* combat; /* combat event callback, fn(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t id, uint64_t revision) */
+	void* imgui; /* id3dd9::present callback, before imgui::render, fn(uint32_t not_charsel_or_loading) */
+	void* options_end; /* id3dd9::present callback, appending to the end of options window in arcdps, fn() */
+	void* combat_local;  /* combat event callback like area but from chat log, fn(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t id, uint64_t revision) */
+	void* wnd_filter; /* wndproc callback like above, input filered using modifiers */
+	void* options_windows; /* called once per 'window' option checkbox, with null at the end, non-zero return disables drawing that option, fn(char* windowname) */
+} arcdps_exports;
+
+/* proto/globals */
+uint32_t cbtcount = 0;
+arcdps_exports arc_exports;
+char* arcvers;
+void dll_init(HANDLE hModule);
+void dll_exit();
+extern "C" __declspec(dllexport) void* get_init_addr(char* arcversionstr, void* imguicontext, void* id3dd9);
+extern "C" __declspec(dllexport) void* get_release_addr();
+bool nothing;
+arcdps_exports* mod_init();
+uintptr_t mod_release();
+uintptr_t mod_options();
+
+/* dll main -- winapi */
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ulReasonForCall, LPVOID lpReserved) {
+	switch (ulReasonForCall) {
+		case DLL_PROCESS_ATTACH: dll_init(hModule); break;
+		case DLL_PROCESS_DETACH: dll_exit(); break;
+
+		case DLL_THREAD_ATTACH:  break;
+		case DLL_THREAD_DETACH:  break;
+	}
+	return 1;
+}
+
+/* dll attach -- from winapi */
+void dll_init(HANDLE hModule) {
+	return;
+}
+
+/* dll detach -- from winapi */
+void dll_exit() {
+	return;
+}
+
+/* export -- arcdps looks for this exported function and calls the address it returns on client load */
+extern "C" __declspec(dllexport) void* get_init_addr(char* arcversionstr, void* imguicontext, void* id3dd9) {
+	arcvers = arcversionstr;
+	ImGui::SetCurrentContext((ImGuiContext*)imguicontext);
+	return mod_init;
+}
+
+/* export -- arcdps looks for this exported function and calls the address it returns on client exit */
+extern "C" __declspec(dllexport) void* get_release_addr() {
+	arcvers = 0;
+	return mod_release;
+}
+
+/* initialize mod -- return table that arcdps will use for callbacks */
+arcdps_exports* mod_init() {
+	/* for arcdps */
+	memset(&arc_exports, 0, sizeof(arcdps_exports));
+	arc_exports.sig = 0x66;
+	arc_exports.imguivers = IMGUI_VERSION_NUM;
+	arc_exports.size = sizeof(arcdps_exports);
+	arc_exports.out_name = "combatdemo";
+	arc_exports.out_build = "0.1";
+	arc_exports.options_end = mod_options;
+	return &arc_exports;
+}
+
+/* release mod -- return ignored */
+uintptr_t mod_release() {
+	FreeConsole();
+	return 0;
+}
+
+uintptr_t mod_options() {
+	if (ImGui::BeginMenu("test")) {
+		ImGui::Checkbox("test", &nothing);
+		ImGui::EndMenu();
+	}
+
+	return 0;
+}
